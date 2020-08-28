551deb14f539bc832eb40681a3d19ccc03e86127
Maksim Baravy <35226420+maximkatut@users.noreply.github.com>
2020-08-28 16:43:47 -0400

    install react helmet and react-slick

--- src/index.js
+++ src/index.js
@@ -7,11 +7,9 @@ import App from "./containers/App";
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById("root")
 );
 
