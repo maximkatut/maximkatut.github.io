5b842f9cc20b8eb56cd4c6ddc54013c1dc849790
Maksim Baravy <35226420+maximkatut@users.noreply.github.com>
2020-08-29 10:26:40 -0400

    change about section info

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -4,15 +4,35 @@ const About = () => {
   return (
     <section className="about">
       <h2 className="about__title">About me</h2>
-      <p className="about__description">
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Suscipit
-        quaerat odit nihil id totam sit adipisci omnis soluta impedit,
-        architecto dignissimos maiores mollitia! Maiores voluptates ratione
-        voluptate odio ut. Nisi aliquam earum quasi cum id voluptates debitis
-        ratione rem magnam molestias harum sequi voluptate soluta vel facere,
-        magni vitae explicabo labore hic dolores alias assumenda deserunt error
-        placeat? Iste!
-      </p>
+      <div className="about__description">
+        Hi!{" "}
+        <span role="img" aria-label="hi emoji">
+          🙋‍♂️
+        </span>{" "}
+        My name is Max Baravy. I'm from Belarus 🇧🇾 Currently living in the
+        United States 🇺🇸. I am front end developer with a strong passion of
+        ReactJS framework. Each day I learn something new and create web
+        products using best practices and modern techhologies.
+        <div className="about__stack">
+          <span role="img" aria-label="hi emoji">
+            🛠
+          </span>{" "}
+          My tech stack:
+          <ul className="about__list">
+            <li className="about__item">
+              Responsive web-development: HTML5, CSS/SCSS
+            </li>
+            <li className="about__item">Frontend: JavaScript (ES5/ES6)</li>
+            <li className="about__item">
+              JavaScript libraries: React, Redux, TypeScript
+            </li>
+            <li className="about__item">Testing libraries: Jest, Enzyme</li>
+            <li className="about__item">Code bundlers: Gulp, Webpack</li>
+            <li className="about__item">UX/UI: Adobe Photoshop, Figma</li>
+            <li className="about__item">Version-control system: Git</li>
+          </ul>
+        </div>
+      </div>
     </section>
   );
 };
