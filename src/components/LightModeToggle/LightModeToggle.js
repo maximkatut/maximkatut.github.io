4b5bfb3275f93ac50cb6f991838b17dffd9b7a93
Maksim Baravy <35226420+maximkatut@users.noreply.github.com>
2020-09-06 10:11:49 -0400

    make a new toggle for dark mode

--- src/components/LightModeToggle/LightModeToggle.js
+++ src/components/LightModeToggle/LightModeToggle.js
@@ -4,19 +4,36 @@ import { connect } from "react-redux";
 
 import { ActionCreator } from "../../redux/actions";
 
-const Toggle = styled.span`
+const ToggleBox = styled.div`
+  z-index: 0;
+  padding: 4px 3px 0px;
   position: absolute;
   left: 200px;
   top: 10px;
   cursor: pointer;
-  transition: transform 0.1s ease-in;
-
-  &:hover {
-    transform: scale(1.3);
-  }
+  transition: all 0.1s ease-in;
+  width: 55px;
+  border-radius: 20px;
+  background-color: ${(props) => (props.lightMode ? `#999` : `#555`)};
 
   @media (max-width: 900px) {
-    left: 60px;
+    display: none;
+  }
+`;
+
+const Toggle = styled.div`
+  position: absolute;
+  top: 3px;
+  left: ${(props) => (props.lightMode ? `3px` : `33px`)};
+  width: 25px;
+  height: 25px;
+  background-color: #fff;
+  border-radius: 50%;
+  transition: left 0.1s ease-in;
+
+  &:focus {
+    box-shadow: 0 0 2px 3px rgba(199, 47, 47, 0.8);
+    outline: none;
   }
 `;
 
@@ -43,15 +60,31 @@ class LightModeToggle extends Component {
   render() {
     const { lightMode, toggleLightMode } = this.props;
     return (
-      <Toggle
+      <ToggleBox
         role="img"
+        lightMode={lightMode}
         aria-label="light mode"
         onClick={() => {
           toggleLightMode(!lightMode);
+          this.toggle.focus();
+        }}
+        onKeyPress={(evt) => {
+          evt.preventDefault();
+          if (evt.key === `Enter` || ` `) {
+            toggleLightMode(!lightMode);
+            this.toggle.focus();
+          }
         }}
       >
-        {lightMode ? "🌚" : "🌝"}
-      </Toggle>
+        <span role="img" aria-label="dark mode">
+          🌝 🌚
+        </span>
+        <Toggle
+          tabIndex="0"
+          lightMode={lightMode}
+          ref={(toggle) => (this.toggle = toggle)}
+        ></Toggle>
+      </ToggleBox>
     );
   }
 }
