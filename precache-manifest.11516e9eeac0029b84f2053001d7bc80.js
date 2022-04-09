5675385e7e8d20e2079c9d3bcef8cc5fb99d8fdd
Max Baravy <maximkatut@gmail.com>
2022-04-09 09:59:02 -0400

    Updates

---
+++ precache-manifest.11516e9eeac0029b84f2053001d7bc80.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6779e3e645fe777262177f5d61d23377",
+    "url": "/maximkatut.github.io/index.html"
+  },
+  {
+    "revision": "10f3d4bdef8f2f84d980",
+    "url": "/maximkatut.github.io/static/css/2.0635a70e.chunk.css"
+  },
+  {
+    "revision": "5f95708ec2cd8f857627",
+    "url": "/maximkatut.github.io/static/css/main.b346a6b2.chunk.css"
+  },
+  {
+    "revision": "10f3d4bdef8f2f84d980",
+    "url": "/maximkatut.github.io/static/js/2.ea96f063.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/maximkatut.github.io/static/js/2.ea96f063.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f95708ec2cd8f857627",
+    "url": "/maximkatut.github.io/static/js/main.767962bd.chunk.js"
+  },
+  {
+    "revision": "69d5bfdaf144b01130ad",
+    "url": "/maximkatut.github.io/static/js/runtime-main.4b8ff8c9.js"
+  },
+  {
+    "revision": "83bcd4bda4c77a6e32239b60bc1b6bf7",
+    "url": "/maximkatut.github.io/static/media/cinema.83bcd4bd.jpg"
+  },
+  {
+    "revision": "a8193003cbd2d34d663eaa208556b2b0",
+    "url": "/maximkatut.github.io/static/media/facebook-white.a8193003.svg"
+  },
+  {
+    "revision": "6bb68eec076c1b7062cf2ad556ea6ab2",
+    "url": "/maximkatut.github.io/static/media/facebook.6bb68eec.svg"
+  },
+  {
+    "revision": "456a86bb03a5f38d129ab941569195e1",
+    "url": "/maximkatut.github.io/static/media/gllacy.456a86bb.jpg"
+  },
+  {
+    "revision": "74147f92fb08fefd5892fd0011ec28da",
+    "url": "/maximkatut.github.io/static/media/heart.74147f92.svg"
+  },
+  {
+    "revision": "38bb03ea814351e54db70cb66c1387ea",
+    "url": "/maximkatut.github.io/static/media/linkedin-white.38bb03ea.svg"
+  },
+  {
+    "revision": "25d9550dbea7e0df8c0ebb1ed377b782",
+    "url": "/maximkatut.github.io/static/media/linkedin.25d9550d.svg"
+  },
+  {
+    "revision": "111ed7c5c20a6c3e6f9725fe76a19354",
+    "url": "/maximkatut.github.io/static/media/polind.111ed7c5.jpg"
+  },
+  {
+    "revision": "2d9d9ac9cd0fd4a0306e665cc26606df",
+    "url": "/maximkatut.github.io/static/media/sedona.2d9d9ac9.jpg"
+  },
+  {
+    "revision": "bed653cf4b93109889527e5ba46479cd",
+    "url": "/maximkatut.github.io/static/media/six-cities.bed653cf.jpg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/maximkatut.github.io/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/maximkatut.github.io/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/maximkatut.github.io/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/maximkatut.github.io/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "fb34984f9b2cbb0070f1234cbc9d0bc3",
+    "url": "/maximkatut.github.io/static/media/twitter-white.fb34984f.svg"
+  },
+  {
+    "revision": "48250d935a880294469913616390a656",
+    "url": "/maximkatut.github.io/static/media/twitter.48250d93.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8e1dc698c8e12162893a08f0a694e284.js"
+  "/maximkatut.github.io/precache-manifest.11516e9eeac0029b84f2053001d7bc80.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/maximkatut.github.io/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
